f077810dfa1e0e90efaa16167efba50f0ae5ccca
S3B20 <62393273+S3B20@users.noreply.github.com>
2022-12-24 00:51:23 +0100

    testing

---
+++ src/test/java/trio/a/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package trio.a.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
