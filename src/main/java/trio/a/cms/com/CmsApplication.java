1dc62f22238a85343ce2a3bbda3714e19b6b6fe2
Eddi1405 <edwardsiebert53@gmail.com>
2023-02-05 08:48:38 +0100

    test

--- src/main/java/trio/a/cms/com/CmsApplication.java
+++ src/main/java/trio/a/cms/com/CmsApplication.java
@@ -1,9 +1,11 @@
-package trio.a.cms;
+package trio.a.cms.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "trio.a.cms.controller")
 public class CmsApplication {
 
 	public static void main(String[] args) {
