383042f6afd30d2b6a0ee9faf4f9e9e810974191
Eddi1405 <edwardsiebert53@gmail.com>
2023-02-24 23:16:49 +0100

    design changes

--- src/main/java/de/trio/imageshare/web/SampleController.java
+++ src/main/java/de/trio/imageshare/web/SampleController.java
@@ -1,7 +1,6 @@
 package de.trio.imageshare.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class SampleController {
 
-    @GetMapping(value = "/index")    public String getIndexPage(Model model) {
-        log.debug("entering getIndexPage");
+    @GetMapping(value = "/index")
+    public String getIndexPage() {
         return "index";
     }
 
@@ -21,8 +20,7 @@ public class SampleController {
     }
 
     @GetMapping("/register")
-    public String onOpenUrl() {
-        log.info("entering onOpenUrl (GET-Method /register)");
+    public String getRegisterPage() {
         return "register";
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE IF NOT EXISTS users(
     username varchar(50) NOT NULL,
     email varchar(50) NOT NULL
 ) ;
+
+CREATE TABLE Data(
+    Bild varchar(50) NOT NULL,
+    Title varchar(50),
+    Beschreibung varchar(50),
+    Katiegorie varchar(20),
+    Zeit int NOT NULL
+    ) ;
