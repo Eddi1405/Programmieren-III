8b8f5e8307f0d851ac2a4312f7ad475a0b01f895
Seb <sebastianstein64@gmail.com>
2023-02-26 17:24:00 +0100

    jakarta Dependecy

--- src/main/java/de/trio/imageshare/ImageShareApplication.java
+++ src/main/java/de/trio/imageshare/ImageShareApplication.java
@@ -6,6 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImageShareApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ImageShareApplication.class, args);
-    }
+        SpringApplication.run(ImageShareApplication.class, args);}
     }
