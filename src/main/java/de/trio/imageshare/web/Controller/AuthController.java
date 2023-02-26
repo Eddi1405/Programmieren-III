a62564cc7a67d5566b4cd0b34f4777b59f27f907
Seb <sebastianstein64@gmail.com>
2023-02-26 17:34:26 +0100

    Login ID & Register ID Match, filled AuthController mit Routing für Custom Login Page

--- src/main/java/de/trio/imageshare/web/Controller/AuthController.java
+++ src/main/java/de/trio/imageshare/web/Controller/AuthController.java
@@ -1,4 +1,11 @@
 package de.trio.imageshare.web.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class AuthController {
+    //Loginrequest Routing
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
