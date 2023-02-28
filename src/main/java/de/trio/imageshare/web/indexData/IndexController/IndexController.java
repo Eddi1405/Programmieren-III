bca9b88dee524f5f38174dc62cf85fbd6414060a
Eddi1405 <edwardsiebert53@gmail.com>
2023-02-28 12:15:55 +0100

    BIlder werdne hochgeladen

--- src/main/java/de/trio/imageshare/web/indexData/IndexController/IndexController.java
+++ src/main/java/de/trio/imageshare/web/indexData/IndexController/IndexController.java
@@ -6,16 +6,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class IndexController {
     @Autowired
     private DataRepository dataRepository;
 
+    Path pfad;
+
     @PostMapping("/index")
-    public String addPicture(@RequestParam String bildpfad, @RequestParam String title, @RequestParam String beschreibung, @RequestParam String kategorie, @RequestParam Integer zeit){
+    public String addPicture(@RequestParam("bildpfad")MultipartFile bildpfad, @RequestParam String title, @RequestParam String beschreibung, @RequestParam String kategorie, @RequestParam Integer zeit)throws IOException{
         Data data = new Data();
-        data.setBildpfad(bildpfad);
+        if(!bildpfad.isEmpty()){
+            byte[] bildpfadBytes = bildpfad.getBytes();
+            pfad = Paths.get("src/main/resources/pictures_save/" + bildpfad.getOriginalFilename());
+
+
+            try{
+                BufferedImage image = ImageIO.read(new ByteArrayInputStream(bildpfadBytes));
+                ImageIO.write(image,"png",pfad.toFile());
+            }catch (IOException e){
+
+            }
+
+        }
+        data.setBildpfad(String.valueOf(pfad.toFile()));
         data.setTitle(title);
         data.setBeschreibung(beschreibung);
         data.setKategorie(kategorie);
@@ -23,5 +47,6 @@ public class IndexController {
         dataRepository.save(data);
         return "/index";
     }
+    }
 
-}
+
--- src/main/java/de/trio/imageshare/web/indexData/IndexModel/DataRepository.java
+++ src/main/java/de/trio/imageshare/web/indexData/IndexModel/DataRepository.java
@@ -3,6 +3,9 @@ package de.trio.imageshare.web.indexData.IndexModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Ist dafür da das man aktionen in der datenbank ausführen kann
+ */
 @Repository
 public interface DataRepository extends JpaRepository<Data, Long> {
 }
