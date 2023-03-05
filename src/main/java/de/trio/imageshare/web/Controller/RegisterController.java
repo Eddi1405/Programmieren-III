c3fe26bd5d912dfd515c200cd5b421d7578506b8
Flo7880 <florian-laufer@web.de>
2023-03-05 16:46:05 +0100

    Registration

--- src/main/java/de/trio/imageshare/web/Controller/RegisterController.java
+++ src/main/java/de/trio/imageshare/web/Controller/RegisterController.java
@@ -1,13 +1,47 @@
 package de.trio.imageshare.web.Controller;
+
+import de.trio.imageshare.web.Repository.UserDaten;
+import de.trio.imageshare.web.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import lombok.extern.slf4j.Slf4j;
-import lombok.Data;
-@Slf4j
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Objects;
+
+
+@Controller
 public class RegisterController {
-    //Loginrequest Routing
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+
     @GetMapping("/register")
-    public String onOpenUrl() {
-        log.info("entering onOpenUrl (GET-Method /register)");
-        return "register";
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new UserDaten());
+
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(UserDaten user) {
+        //BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        //String encodedPassword = passwordEncoder.encode(user.getPassword());
+        //user.setPassword(encodedPassword);
+        if(Objects.equals(user.getPassword(), user.getPassword2())){
+            userRepo.save(user);
+            return "register_success";
+        }else{
+            return "register_error";
+        }
+
+
+
     }
 }
