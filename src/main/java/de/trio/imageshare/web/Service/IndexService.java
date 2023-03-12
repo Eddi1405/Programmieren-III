1dea1f3686dee395acd3e14d1ab9c202606e68b1
Eddi1405 <edwardsiebert53@gmail.com>
2023-03-12 14:21:56 +0100

    bild wird nach bestimmter zeit gelöscht

--- src/main/java/de/trio/imageshare/web/Service/IndexService.java
+++ src/main/java/de/trio/imageshare/web/Service/IndexService.java
@@ -3,17 +3,27 @@ package de.trio.imageshare.web.Service;
 import de.trio.imageshare.web.entities.PictureDaten;
 import de.trio.imageshare.web.Repository.PictureRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 
 
 @Service
 public class IndexService {
     private final PictureRepository dataInterface;
+    private final PlatformTransactionManager transactionManager;
+    private final Timer timer;
 
-    public IndexService(PictureRepository dataInterface) {
+    public IndexService(PictureRepository dataInterface, PlatformTransactionManager transactionManager) {
         this.dataInterface = dataInterface;
+        this.transactionManager = transactionManager;
+        this.timer = new Timer();
     }
 
     public void saveData(String name, MultipartFile file, String title, String beschreibung, String kategorie, int zeit) throws IOException {
@@ -26,10 +36,30 @@ public class IndexService {
         data.setKategorie(kategorie);
         data.setZeit(zeit);
         dataInterface.save(data);
+        if(zeit != 99){
+            deleteDataAfter(name,zeit*60,dataInterface);
+        }
     }
 
     public PictureDaten getNamebybildname(String bildname, PictureRepository pictureRepository) {
         return pictureRepository.findBybildname(bildname).orElse(null);
     }
 
+    public void deleteDataAfter(String bildname, int delay, PictureRepository pictureRepository) {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                //wird verwendet, um die Löschoperation in einer Transaktion zu kapseln und sicherzustellen
+                TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+                transactionTemplate.execute(status -> {
+                    PictureDaten pictureDaten = pictureRepository.findBybildname(bildname).orElse(null);
+                    if (pictureDaten != null) {
+                        pictureRepository.delete(pictureDaten);
+                    }
+                    return null;
+                });
+            }
+        };
+        timer.schedule(task, delay * 1000);
+    }
 }
